4d23def25a2f5c60b2534e0476ca61c7ce23bfe8
Héctor Muñoz <hectormunozgarcia@gmail.com>
2017-01-01 17:10:11 +0000

    first rules

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('hola')
+function Player(name) {
+	this.name  = name;
+	this.hp    = 100;
+	this.sp    = 100;
+
+	this.heal = function( targetPlayer ) {
+		if (this.sp >= 40) {
+			this.sp -= 40;
+			targetPlayer.hp += 20;	
+		} else {
+			console.info(this.name + " no tiene sp suficientes.")
+		}
+		this.status(targetPlayer);	
+	}
+
+	this.attack = function( targetPlayer ) {
+		if ( targetPlayer.hp > 40 ) {
+			targetPlayer.hp -= 40;
+			this.status(targetPlayer);	
+		} else {
+			targetPlayer.hp = 0;
+			console.error(targetPlayer.name + " ha muerto.")
+		}
+
+	}
+
+	this.status = function( targetPlayer ) {
+		console.info( this );
+		console.info( targetPlayer );
+	}
+}
+
+var gandalf = new Player('Gandalf');
+var legolas = new Player('Legolas');	
+
+
